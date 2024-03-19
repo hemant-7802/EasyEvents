61896c569b25725e142927e3cce8b52e6b3b2a0a
karan0617 <mrlonewolf1774@gmail.com>
2024-03-19 13:12:42 +0530

    About us Added

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Map_contact from "./components/map & contact/Map_contact";
 import Team from "./components/ourteam/Team";
 import Review from "./components/Customer_Review/Review";
 import Footer from "./components/footer/Footer";
+import About from "./components/about us/About"
 // import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
@@ -14,6 +15,7 @@ export default function App() {
       <Nav />
       <Heropage />
       <WhyUs />
+      <About />
       <Team />
       <Review />
       <Map_contact />
