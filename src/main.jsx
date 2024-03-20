760d6b286c30ff097652695d0d7f583c53f5ddc9
Hemant Bhargav <hsharma9667@gmail.com>
2024-03-20 16:56:03 +0530

    Delete WhiteSpace

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <NextThemesProvider>
       <App />
     </NextThemesProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
   </GoogleOAuthProvider>
 )
